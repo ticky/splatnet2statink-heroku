d98290a766664b44edac3451bf14f70d678ead3d
Jessica Stokes <hello@jessicastokes.net>
2018-07-22 16:28:37 -0700

    pushdpopd

--- bin/update.sh
+++ bin/update.sh
@@ -4,8 +4,10 @@ export AWS_ACCESS_KEY_ID="$CLOUDCUBE_ACCESS_KEY_ID"
 export AWS_SECRET_ACCESS_KEY="$CLOUDCUBE_SECRET_ACCESS_KEY"
 export AWS_BUCKET=$(basename "$CLOUDCUBE_URL")
 
-aws s3 sync "s3://$AWS_BUCKET" ./private
+aws s3 sync "s3://$AWS_BUCKET" ./config
 
-./splatnet2statink/splatnet2statink.py -r
+pushd ./config
+  ../splatnet2statink/splatnet2statink.py -r
+popd
 
-aws s3 sync ./private "s3://$AWS_BUCKET"
+aws s3 sync ./config "s3://$AWS_BUCKET"
